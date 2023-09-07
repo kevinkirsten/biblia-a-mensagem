9fc44be92a9adba9c5b76d2a435bbb4d1c45f0cf
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2023-09-07 01:17:26 -0300

    update microsoft clarity project id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "ir8vzxo622");`,
+          })(window, document, "clarity", "script", "ir96aw2g7s");`,
         }}
       />
       <body
