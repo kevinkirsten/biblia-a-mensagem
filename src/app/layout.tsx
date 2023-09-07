ca1b0680741eb44d94b35d97e5ccf3d3e8244791
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2023-09-07 15:56:12 -0300

    refactor: integrate microsoft clarity project id as environment variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "ir96aw2g7s");`,
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY_PROJECT_ID}");`,
         }}
       />
       <body
