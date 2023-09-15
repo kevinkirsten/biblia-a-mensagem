881ac609282683beb89d075c762e1a3a8d54ec92
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2023-09-15 19:08:23 -0300

    fix nav buttons position on chapter page

--- src/components/Verses/index.tsx
+++ src/components/Verses/index.tsx
@@ -64,16 +64,16 @@ export default function Verses({
         ))}
       </article>
       <nav
-        className="flex items-center justify-between rounded-b-lg border-t border-gray-200 bg-white px-4 py-3 dark:border-gray-700 dark:bg-gray-900 sm:px-6"
+        className="flex flex-col gap-2 sm:gap-0 sm:flex-row items-start sm:items-center justify-between rounded-b-lg border-t border-gray-200 bg-white px-4 py-3 dark:border-gray-700 dark:bg-gray-900 sm:px-6"
         aria-label="Pagination"
       >
-        <div className="hidden sm:block">
+        <div className="sm:block">
           <p className="text-sm text-gray-700 dark:text-gray-400">
             Capítulo <span className="font-medium">{chapter}</span> de{" "}
             <span className="font-medium">{book?.chaptersCount}</span>
           </p>
         </div>
-        <div className="flex flex-1 justify-between gap-2 sm:justify-end">
+        <div className="flex w-full flex-1 justify-between gap-2 sm:justify-end">
           <Button
             variant="outline"
             title="Voltar para o topo"
@@ -81,12 +81,14 @@ export default function Verses({
           >
             <ArrowUpToLine className="h-5 w-5" />
           </Button>
-          <Button variant="outline" disabled={!previousBookExists}>
-            <Link href={previousBookUrl}>Anterior</Link>
-          </Button>
-          <Button variant="outline" disabled={!nextBookExists}>
-            <Link href={nextBookUrl}>Próximo</Link>
-          </Button>
+          <div className="flex gap-2">
+            <Button variant="outline" disabled={!previousBookExists}>
+              <Link href={previousBookUrl}>Anterior</Link>
+            </Button>
+            <Button variant="outline" disabled={!nextBookExists}>
+              <Link href={nextBookUrl}>Próximo</Link>
+            </Button>
+          </div>
         </div>
       </nav>
     </>
