de8769b0a27881d46cfcafaa0ca0a4a91bc6a8fd
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2024-01-29 21:59:02 -0300

    add site url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const isProduction = process.env.VERCEL_ENV === "production";
-  const siteUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
   if (isProduction) {
     return {
